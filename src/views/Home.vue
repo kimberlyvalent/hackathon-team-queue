be6e573b314121762b72705a6e1537f7ab54a535
James Behr <jbehr@2u.com>
2020-05-08 14:15:45 +0200

    Add basic routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Welcome</h1>
+    <div>
+      <sub>To join a queue, enter the queue ID here</sub>
+    </div>
+    <form @submit.prevent="joinQueue">
+      <div>
+        <input inputmode="numeric" type="text" v-model="code">
+      </div>
+      <button type="submit">Join</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      code: '',
+    };
+  },
+  methods: {
+    joinQueue() {
+      this.$router.push(`/queue/${this.code}`);
+    },
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
---
+++ src/views/Queue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="queue">
+    <router-link to="/">Back</router-link>
+    <h1>Joining queue #{{ id }}</h1>
+    <div>Loading&hellip;</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Queue",
+  props: ["id"],
+  mounted() {
+    // TODO: We can attempt to join the queue here. If the REST API responds
+    // 404, we can display a "Queue Not Found" error, otherwise we can commence
+    // polling for queue updates.
+    console.log("Dispatch join", this.id);
+  }
+};
+</script>
